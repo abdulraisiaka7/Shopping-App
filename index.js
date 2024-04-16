12fe6ded2606ee59f65a6cb15af7c20f42aa6d93
Isiaka Abdulrasheed <isiakaabdulrasheed98@gmail.com>
2024-04-16 17:17:52 +0100

    connect to mongodb and added to dotenv

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require ("express")
+const mongoose = require ("mongoose")
+const dotenv = require ("dotenv")
 
 const app = express()
 const port = 3000
+
+dotenv.config()
+//to access the env file, use process.env.(name of the file)
+
+mongoose.connect("process.env.MONGOOSE_PASSWORD").then(() => {
+    console.log("mongodb connected")
+})
 
 app.listen(port, () => {
     console.log("Express server is running on port 3000")
